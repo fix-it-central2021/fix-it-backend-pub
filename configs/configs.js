de5685c13dd7816671bceb949b909527d98fd6dc
David Godinez <godinezjosue@hotmail.com>
2021-07-10 20:31:56 -0600

    configs

---
+++ configs/configs.js
@@ -0,0 +1,6 @@
+module.exports = {
+    llave: "this_key_is_ultra_secret_xdxdxd_123456",
+    // string_conexion: "mongodb+srv://sa:14090310Jos@cluster0.qnbic.mongodb.net/DB_FIX_IT?retryWrites=true&w=majority"
+
+     string_conexion: "mongodb://administrador:12345678@fix-it-database.cluster-cz0r4od7tlp9.us-east-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-
 const express = require("express");
 const app = express();
+var cors = require('cors')
 app.use(express.json());
 
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 
 
 const repuestos_todos = require('./routes/repuestos_todos')
 
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,access-token,access-user");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE , PUT");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE , PUT, OPTIONS");
     next();
   });
+  
+
   
 app.use('/repuestos',repuestos_todos)
 
