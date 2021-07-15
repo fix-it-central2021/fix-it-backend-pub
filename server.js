9b84548d535492bb901aa8f54066bf767e4b2c81
fix-it-central2021 <86719392+fix-it-central2021@users.noreply.github.com>
2021-07-14 19:09:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require("./index")
 
 const PORT = process.env.PORT || 3800
-app.listen(PORT,()=> console.info(`El servidor se inicio en el puerto ${PORT}`))
+app.listen(PORT,()=> console.info(`El server se inicio en el puerto ${PORT}`))
